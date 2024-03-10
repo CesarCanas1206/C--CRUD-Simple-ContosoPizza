6e2e1a76645dfff78ca405d74cf45a2d051c468e
Oskar0112 <oskardev0112@gmail.com>
2024-03-10 11:13:32 -0400

    initial contosopizza

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,72 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        // This code will save the pizza and return a result
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        // This code will update the pizza and return a result
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        // This code will delete the pizza and return a result
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C--CRUD-Simple-ContosoPizza
 This is boilterplate for CRUD in ASP.NET
+
+For the ASP.NET beginner, This would be helpful.
